4e1f3b92ede2726be48f5f6fa36077d8dd6fd28d
Masaya Sato <sato.masaya.dev@gmail.com>
2022-08-28 14:40:45 +0900

    Swap filenames

--- linux/src/cat_buffered.c
+++ linux/src/cat_buffered.c
@@ -1,47 +1,33 @@
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
-#define BUFFER_SIZE 2048
-
-static void exec_cat(const char *filepath);
+static void exec_cat(FILE *fp);
 static void kill(const char *s);
 
 int main(int argc, char *argv[]) {
   int i;
 
-  // Terminate with error code 1 if failed to get one or more
-  // filenames
-  if (argc < 2) {
-    fprintf(stderr, "%s: filename not given\n", argv[0]);
-    exit(1);
+  // Output chars read from standard input if no valid filename is given
+  if (argc == 1) {
+    exec_cat(stdin);
+  } else {
+    for (i = 1; i < argc; i++) {
+      FILE *fp;
+      fp = fopen(argv[i], "r");
+      if (!fp) kill(argv[i]);
+      exec_cat(fp);
+      fclose(fp);
+    }
+    exit(0);
   }
-
-  for (i = 1; i < argc; i++) exec_cat(argv[i]);
-  exit(0);
 }
 
-static void exec_cat(const char *filepath) {
-  int fd, m, n;
-  unsigned char buf[BUFFER_SIZE];
+static void exec_cat(FILE *fp) {
+  int c;
 
-  fd = open(filepath, O_RDONLY);
-  // Terminate if failed to open file properly
-  if (fd < 0) kill(filepath);
-  for (;;) {
-    m = read(fd, buf, sizeof buf);
-    // Terminate if failed to read from file properly
-    if (m < 0) kill(filepath);
-    // Terminate if reached end-of-file
-    if (m == 0) break;
-    n = write(STDOUT_FILENO, buf, m);
-    // Terminate if failed to write to stdout properly
-    if (n < 0) kill(filepath);
+  while ((c = fgetc(fp)) != EOF) {
+    if (putchar(c) < 0) exit(1);
   }
-  if (close(fd) < 0) kill(filepath);
 }
 
 static void kill(const char *s) {
--- linux/src/cat_unbuffered.c
+++ linux/src/cat_unbuffered.c
@@ -1,33 +1,47 @@
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-static void exec_cat(FILE *fp);
+#define BUFFER_SIZE 2048
+
+static void exec_cat(const char *filepath);
 static void kill(const char *s);
 
 int main(int argc, char *argv[]) {
   int i;
 
-  // Output chars read from standard input if no valid filename is given
-  if (argc == 1) {
-    exec_cat(stdin);
-  } else {
-    for (i = 1; i < argc; i++) {
-      FILE *fp;
-      fp = fopen(argv[i], "r");
-      if (!fp) kill(argv[i]);
-      exec_cat(fp);
-      fclose(fp);
-    }
-    exit(0);
+  // Terminate with error code 1 if failed to get one or more
+  // filenames
+  if (argc < 2) {
+    fprintf(stderr, "%s: filename not given\n", argv[0]);
+    exit(1);
   }
+
+  for (i = 1; i < argc; i++) exec_cat(argv[i]);
+  exit(0);
 }
 
-static void exec_cat(FILE *fp) {
-  int c;
+static void exec_cat(const char *filepath) {
+  int fd, m, n;
+  unsigned char buf[BUFFER_SIZE];
 
-  while ((c = fgetc(fp)) != EOF) {
-    if (putchar(c) < 0) exit(1);
+  fd = open(filepath, O_RDONLY);
+  // Terminate if failed to open file properly
+  if (fd < 0) kill(filepath);
+  for (;;) {
+    m = read(fd, buf, sizeof buf);
+    // Terminate if failed to read from file properly
+    if (m < 0) kill(filepath);
+    // Terminate if reached end-of-file
+    if (m == 0) break;
+    n = write(STDOUT_FILENO, buf, m);
+    // Terminate if failed to write to stdout properly
+    if (n < 0) kill(filepath);
   }
+  if (close(fd) < 0) kill(filepath);
 }
 
 static void kill(const char *s) {
